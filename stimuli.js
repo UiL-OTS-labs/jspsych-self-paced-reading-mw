94c32c21ed089f2586c44e1856f99ecfa6b8e866
Jacco van Elst <j.c.vanelst@uu.nl>
2020-11-03 19:27:35 +0100

    last statements about this (dev work) https://www.youtube.com/watch?v=08wtZJc-Yic

--- stimuli.js
+++ stimuli.js
@@ -6,11 +6,12 @@ const PRACTICE_ITEMS = [
         condition: "undefined",
         id: 1,
         words: "=============================================<BR><BR>" + 
-               "In/het/tuincentrum/konden/Martine/en/Marije<BR>/"      + 
-               "genoeg#van#hun#gading#vinden./Maar/ze/wilden<BR>/"     +
+               "In/het/tuincentrum/konden/Martine/en/Marije/<BR>"      + 
+               "genoeg#van#hun#gading#vinden./Maar/ze/wilden/<BR>"     +
                "graag/binnen/de/lijntjes/kleuren,/dus/moest<BR>/"      +
                "er/wel/flink/nagedacht/worden#over#deze<BR>#"          +
-               "lijntjes.<BR>",
+               "lijntjes.<BR><BR>"                                     +
+               "=============================================<BR><BR>",
         question_or_statement: "Er was niets te vinden in het tuincentrum.",
         choices: ['Yes','No'],
         correct: 1
@@ -22,8 +23,10 @@ const PRACTICE_ITEMS = [
         id: 2,
         words: "=============================================<BR><BR>" +
                "Godelieve/en/Vincent/waren/net/verhuisd/en<BR>"        +
-               "/waren/aan/het/#klussen/#op/#zolder. 'Zo zijn(BVR>"    +
-               "/ze/nu/eenmaal,/die/Godelieve#en#Vincent'<BR>",
+               "/waren/aan/het#klussen/#op#zolder. 'Zo zijn<BR>"       +
+               "/ze/nu/eenmaal,/die#Godelieve#en#Vincent'"             + 
+               "<BR><BR><BR>"                                          +
+               "=============================================<BR><BR>",
         question_or_statement: "Goedelieve en Vincent hebben een klusjesman gehuurd",
         choices: ['Yes','No'],
         correct: 1
